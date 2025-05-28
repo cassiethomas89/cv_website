d1e27f67406298a24ba2590c4c6fa1cb3846e9fc
Cassie <cassieleannethomas@gmail.com>
2025-05-28 22:15:28 +0100

    CV00006 Added footer links

---
+++ components/BottomBanner.tsx
@@ -0,0 +1,8 @@
+const BottomBanner = () => {
+    return <div className="footerLinksList">
+        <a href="https://github.com/cassiethomas89"><img src="github-mark-white.png" alt="GitHubProfile"/></a>
+        <a href="https://www.linkedin.com/in/cassie-thomas-53ba9b114/"><img src="InBug-White.png"/></a>
+    </div>
+}
+
+export default BottomBanner;
