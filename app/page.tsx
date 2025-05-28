7fdab16cf698bae26e6c1cf97484df77631d897d
Cassie <cassieleannethomas@gmail.com>
2025-05-28 15:51:52 +0100

    CV00002 Download buttons now work.

--- app/page.tsx
+++ app/page.tsx
@@ -25,22 +25,26 @@ export default function Home() {
         </div>
         <div className="section sectionOdds" id="Experience">
           <h1>Experience</h1>
+          This section has not yet been added
         </div>
         <div className="section sectionEvens sectionCV" id="CV">
           <h1>CV</h1>
           <p>If you would like copy of my CV you can download that here</p>
           <div className="downloadButtons">
-            <button>PDF</button>
-            <button>Doc</button>
+            <a className="downloadBtn" href="Cassie_Thomas_CV.pdf" target="_blank" rel="noopener noreferrer" download>
+              PDF
+            </a>
+            <a className="downloadBtn" href="Cassie_Thomas_CV.doc" target="_blank" rel="noopener noreferrer" download>Doc</a>
           </div>
         </div>
         <div className="section sectionOdds contact" id="Contact">
           <h1>Contact Me</h1>
-          <textarea></textarea>
+          This section has not yet been added
+          {/* <textarea></textarea>
           <br/>
           <button>submit</button>
-          <br/>
-        </div>
+          <br/>*/}
+        </div> 
       </div>
       <div className="footer topBannerLight"></div>
   </main>
