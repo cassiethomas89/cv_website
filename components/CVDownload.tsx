d7161e786dbbbb40f5f4d2efefe0146c8ff13bf5
Cassie <cassieleannethomas@gmail.com>
2025-06-03 14:19:46 +0100

    CV00009 Changed up some styling

--- components/CVDownload.tsx
+++ components/CVDownload.tsx
@@ -3,8 +3,8 @@ const CVDownload = () => {
           <h1>CV</h1>
           <p>If you would like copy of my CV you can download that here</p>
           <div className="downloadButtons">
-            <a className="downloadBtn sectionEvens" href="Cassie_Thomas_CV.pdf" target="_blank" rel="noopener noreferrer">PDF</a>
-            <a className="downloadBtn sectionEvens" href="Cassie_Thomas_CV.doc" target="_blank" rel="noopener noreferrer">Doc</a>
+            <a className="downloadBtn lightModeDark" href="Cassie_Thomas_CV.pdf" target="_blank" rel="noopener noreferrer">PDF</a>
+            <a className="downloadBtn lightModeDark" href="Cassie_Thomas_CV.doc" target="_blank" rel="noopener noreferrer">Doc</a>
           </div>
         </div>
 }
